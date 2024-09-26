2df7af6619e0df7e8ab2fca14595ed89a94d77ef
Kaan Şenol <kaansnl@icloud.com>
2024-09-26 19:24:38 +0300

    force: off

--- src/db.js
+++ src/db.js
@@ -598,7 +598,7 @@ const db = {
     try {
       await sequelize.authenticate();
       console.log("Database connection established successfully.");
-      await sequelize.sync({ force: true });
+      await sequelize.sync({});
       console.log("Database synchronized successfully.");
     } catch (error) {
       console.error("Unable to connect to the database:", error);
